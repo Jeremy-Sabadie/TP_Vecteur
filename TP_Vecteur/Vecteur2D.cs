428f3dd103998143f04947395487efe0624a5bbd
cda07saba <jeremy.sabadie@2isa.org>
2023-01-18 15:11:37 +0100

    Ajoutez des fichiers projet.

---
+++ TP_Vecteur/Vecteur2D.cs
@@ -0,0 +1,21 @@
+﻿namespace TP_Vecteur
+{
+    internal class Vecteur2D
+    {
+        private int X;
+        private int Y;
+
+        public Vecteur2D(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X1 { get => X; set => X = value; }
+        public int Y1 { get => Y; set => Y = value; }
+        public double Norme(int x, int y)
+        {
+            return Math.Sqrt(Math.Pow(0 - X, 2) + Math.Pow(0 - Y, 2));
+        }
+    }
+}
---
+++ TP_Vecteur/Vecteur3D.cs
@@ -0,0 +1,16 @@
+﻿namespace TP_Vecteur
+{
+    internal class Vecteur3D : Vecteur2D
+
+    {
+        private int _Z;
+        public int Z { get => _Z; set => _Z = value; }
+
+
+        public Vecteur3D(int x, int y, int z) : base(x, y)
+        {
+            int Z = z;
+        }
+
+    }
+}
